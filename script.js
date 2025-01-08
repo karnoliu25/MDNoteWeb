9c3cffe491549fbfd1469244efa669edc4e2c05d
KarnoLiu <karnoliu@163.com>
2025-01-08 14:51:55 +0800

    完成保存功能

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ const vm = new Vue({
   data() {
     return {
       content: "",
+      title: "",
+      notes: JSON.parse(localStorage.getItem("notes")) || [],
+      selectedId: null,
     };
   },
   computed: {
-    notePreview() {
+    // 预览
+    preview() {
       return marked.parse(this.content);
+    },
+  },
+  methods: {
+    // 保存
+    save() {
+      const note = {
+        id: Date.now(),
+        title: this.title,
+        content: this.content,
+      };
+      this.notes.push(note);
+      localStorage.setItem("notes", JSON.stringify(this.notes));
+      console.log(`test`);
     },
   },
 });
